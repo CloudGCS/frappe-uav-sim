6d81d5c85805249e91a19dac189fbd0a940cabec
pehlivan38 <spehlivan38@gmail.com>
2024-02-23 15:08:43 +0300

    temp pass on_aircraft_save - hook of aircraft events
    there is an update on aircraft.certificate - it is an attachment - so please revisit the simulator_service accordingly

--- cgs_uav_sim/cloudgcs_uav_simulator/services/aircraft_service.py
+++ cgs_uav_sim/cloudgcs_uav_simulator/services/aircraft_service.py
@@ -2,6 +2,8 @@ import frappe
 from .simulator_service import SimulatorService
 
 def on_aircraft_save(doc, method):
+    # todo: there is an update on aircraft.certificate - it is an attachment - so please revisit the simulator_service accordingly
+    return False
     if not doc.certificate: return False
     sim_srv = SimulatorService()
     result = sim_srv.create_certificate(doc,doc.is_new())
